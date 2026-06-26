488cc48e75d737f0bb2f0b16bec8cdd73de8cb79
Mohammed <eloudyymohammad@gmail.com>
2026-06-26 16:26:44 +0100

    deployment fix progress

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,14 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => array_filter([
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        'http://localhost:3000',
+    ]),
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => array_filter([
+        env('CORS_ALLOWED_ORIGIN_PATTERN'),
+    ]),
 
     'allowed_headers' => ['*'],
 
